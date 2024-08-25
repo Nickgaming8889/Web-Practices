3b2e8384459d3f579cd3d8d26143249399a724d7
Nickgaming8889 <nickgaming8889@gmail.com>
2024-08-24 18:04:34 -0600

    2024.08.24 18:04

---
+++ Form's Examples/js/script.js
@@ -0,0 +1,14 @@
+function myAlert(name_, score){
+    alert("Hello"+name_+"Score"+score);
+}
+
+var name_ = "Nicholas";
+var lastName = "Caceres";
+var score = 67;
+
+myAlert(name_,score);
+
+function sent() {
+    var email_ = document.forma01.email.value;
+    alert(email_);
+}
